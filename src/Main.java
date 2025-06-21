07293e4feeb65314b39aefe5bb64e68195ab4d22
yaregtlt <redin-yaroslav-v@yandex.ru>
2025-06-21 23:59:45 +0400

    Задание по теме Синтаксис языка и Базовые типы данных

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int number2 = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма = " + (number1 + number2));
+        System.out.println("Разность = " + (number1 - number2));
+        System.out.println("Частное = " + ((double) number1/number2));
         }
     }
